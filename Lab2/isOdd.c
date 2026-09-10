b45d7ae38b2f2347de08fcc819524b5a07fc8729
yunjingma00 <y602m419@ku.edu>
2026-09-10 17:19:58 -0500

    Complete EECS348 Lab 2

---
+++ Lab2/isOdd.c
@@ -0,0 +1,11 @@
+#include "isOdd.h"
+
+#include <stdio.h>
+
+int isOdd(int num)
+{
+	if (num % 2 == 1){
+		 printf("%d is odd.\n", num);
+	}
+	return 1;
+}
---
+++ Lab2/main.c
@@ -0,0 +1,12 @@
+#include "isOdd.h"
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    isOdd(num);
+    return 0;
+}
+
